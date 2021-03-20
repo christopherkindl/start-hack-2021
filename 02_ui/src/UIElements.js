aa6135c462ea999361b29df9104ab2d443be17fb
Yvan Bosshard <yvan@ymb.space>
2021-03-20 15:45:53 +0100

    added submission popup

--- 02_ui/src/UIElements.js
+++ 02_ui/src/UIElements.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './UIElements.css';
-
+import Slider from 'react-rangeslider'
+import 'react-rangeslider/lib/index.css'
+import {OccupancyCar, OccupancySlider} from './Occupancy.js'
 
 class Header extends React.Component {
     render() {
@@ -13,15 +15,154 @@ class Header extends React.Component {
     }
 }
 
+
+
+function Spacer() {
+    return (
+        <div className="spacer">
+        </div>
+    );
+}
+
+function shuffle(array) {
+    var curIdx = array.length;
+    var tmp, rndIdx;
+
+    while (0 !== curIdx) {
+        rndIdx = Math.floor(Math.random() * curIdx);
+        curIdx -= 1;
+
+        tmp = array[curIdx];
+        array[curIdx] = array[rndIdx];
+        array[rndIdx] = tmp;
+    }
+
+    return array;
+}
+
+class OccupancyPopup extends React.Component {
+    state = {
+        submitted: false,
+        color: "green",
+        positions: shuffle([0,1,2,3,4]),
+        visibilities: [false, false, false, false, false]
+    }
+
+    onSliderEvt = val => {
+        switch(val) {
+            case 0:
+                this.setState({
+                    color: "green",
+                    positions: shuffle(this.state.positions),
+                    visibilities: [false, false, false, false, false]
+                });
+                break;
+
+            case 20:
+                this.setState({
+                    color: "green",
+                    visibilities: [true, false, false, false, false]
+                });
+                break;
+
+            case 40:
+                this.setState({
+                    color: "lightgreen",
+                    visibilities: [true, true, false, false, false]
+                });
+                break;
+
+            case 60:
+                this.setState({
+                    color: "orange",
+                    visibilities: [true, true, true, false, false]
+                });
+                break;
+
+            case 80:
+                this.setState({
+                    color: "lightred",
+                    visibilities: [true, true, true, true, false]
+                });
+                break;
+
+            case 100:
+                this.setState({
+                    color: "red",
+                    visibilities: [true, true, true, true, true]
+                });
+                break;
+        }
+
+        
+    }
+
+    submit = () => {
+        this.setState({submitted: true});
+        this.closeSubmit();
+    }
+
+    close = () => {
+        this.props.close(false);
+    }
+
+    closeSubmit = () => {
+        setTimeout(() => {
+            this.close()
+          }, 4000);
+    }
+
+    render() {
+        return(
+            <div className="occContainer">
+                <div className="occWindow">
+                { this.state.submitted ?
+
+                    <div>
+                        <span className="occClose" onClick={this.close}>&times;</span>
+                        <p>Thank you for your feedback.</p>
+                        <br />
+                        <p>You earned 10 points!</p>
+                    </div>
+
+                    :
+
+                    <div>
+                        <span className="occClose" onClick={this.close}>&times;</span>
+                        <p>How full was your parking today?</p>
+                        <div className="occParking">
+                            <OccupancyCar color={this.state.color} visible={this.state.visibilities[this.state.positions[0]]} />
+                            <OccupancyCar color={this.state.color} visible={this.state.visibilities[this.state.positions[1]]} />
+                            <OccupancyCar color={this.state.color} visible={this.state.visibilities[this.state.positions[2]]} />
+                            <OccupancyCar color={this.state.color} visible={this.state.visibilities[this.state.positions[3]]} />
+                            <OccupancyCar color={this.state.color} visible={this.state.visibilities[this.state.positions[4]]} />
+                        </div>
+
+                        <Spacer />
+                        <br />
+                        <OccupancySlider callback={this.onSliderEvt}/>
+                        <div className="occSubmit" onClick={() => this.submit()}>
+                            <p>Submit</p>
+                        </div>
+                    </div>
+                    
+                }
+
+                </div>
+            </div>
+        );
+    }
+}
+
 class SearchBar extends React.Component {
     render() {
-      return(
+        return(
         <div className="searchbar">
-          <img src="/assets/search.png" className="searchIcon"/>
-          <p>Search for P+Rail Location</p>
+            <img src="/assets/search.png" className="searchIcon"/>
+            <p>Search for P+Rail Location</p>
         </div>
-      );
+        );
     }
-  }
+}
 
-export  {Header, SearchBar};
+export  {Header, OccupancyPopup, SearchBar};
